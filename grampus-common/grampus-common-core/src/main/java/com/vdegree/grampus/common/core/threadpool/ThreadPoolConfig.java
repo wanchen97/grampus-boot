9bfda661077e9171fc4ce28e9c6c83099522e440
Adrian <297513931@qq.com>
2020-12-05 21:35:11 +0800

    :wrench: 添加线程池相关封装.

---
+++ grampus-common/grampus-common-core/src/main/java/com/vdegree/grampus/common/core/threadpool/ThreadPoolConfig.java
@@ -0,0 +1,67 @@
+package com.vdegree.grampus.common.core.threadpool;
+
+import org.apache.commons.lang3.concurrent.BasicThreadFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.ThreadPoolExecutor;
+
+/**
+ * Title: 线程池配置
+ * Company: v-degree
+ *
+ * @author Beck
+ * @date 2020-11-26
+ */
+@Configuration
+public class ThreadPoolConfig {
+
+	/**
+	 * 核心线程池大小
+	 */
+	private static final int CORE_POOL_SIZE = 50;
+	/**
+	 * 最大可创建的线程数
+	 */
+	private static final int MAX_POOL_SIZE = 200;
+	/**
+	 * 队列最大长度
+	 */
+	private static final int QUEUE_CAPACITY = 1000;
+	/**
+	 * 线程池维护线程所允许的空闲时间
+	 */
+	private static final int KEEP_ALIVE_SECONDS = 300;
+
+	/**
+	 * 线程池任务
+	 */
+	@Bean(name = "threadPoolTaskExecutor")
+	public ThreadPoolTaskExecutor threadPoolTaskExecutor() {
+		ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+		executor.setCorePoolSize(CORE_POOL_SIZE);
+		executor.setMaxPoolSize(MAX_POOL_SIZE);
+		executor.setQueueCapacity(QUEUE_CAPACITY);
+		executor.setKeepAliveSeconds(KEEP_ALIVE_SECONDS);
+		// 线程池对拒绝任务(无线程可用)的处理策略
+		executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+		return executor;
+	}
+
+	/**
+	 * 执行周期性或定时任务
+	 */
+	@Bean(name = "scheduledExecutorService")
+	protected ScheduledExecutorService scheduledExecutorService() {
+		return new ScheduledThreadPoolExecutor(CORE_POOL_SIZE,
+				new BasicThreadFactory.Builder().namingPattern("schedule-pool-%d").daemon(true).build()) {
+			@Override
+			protected void afterExecute(Runnable r, Throwable t) {
+				super.afterExecute(r, t);
+			}
+		};
+	}
+}
---
+++ grampus-common/grampus-common-core/src/main/java/com/vdegree/grampus/common/core/utils/SpringContextHolder.java
@@ -0,0 +1,104 @@
+package com.vdegree.grampus.common.core.utils;
+
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
+
+/**
+ * Title: Spring Context 工具类
+ * Company: v-degree
+ *
+ * @author Beck
+ * @date 2020-12-5
+ */
+@Lazy(false)
+@Component
+public class SpringContextHolder implements ApplicationContextAware, DisposableBean {
+    private static ApplicationContext applicationContext = null;
+
+    /**
+     * 取得存储在静态变量中的ApplicationContext.
+     */
+    public static ApplicationContext getApplicationContext() {
+        return applicationContext;
+    }
+
+    /**
+     * 实现ApplicationContextAware接口, 注入Context到静态变量中.
+     */
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) {
+        SpringContextHolder.applicationContext = applicationContext;
+    }
+
+    /**
+     * 从静态变量applicationContext中取得Bean, 自动转型为所赋值对象的类型.
+     */
+    @SuppressWarnings("unchecked")
+    public static <T> T getBean(String name) {
+        return (T) applicationContext.getBean(name);
+    }
+
+    /**
+     * 从静态变量applicationContext中取得Bean，根据类型.
+     */
+    public static <T> T getBean(Class<T> requiredType) {
+        return applicationContext.getBean(requiredType);
+    }
+
+    /**
+     * 从静态变量applicationContext中取得Bean, 自动转型为参数中的对象类型.
+     */
+    public static <T> T getBean(String name, Class<T> requiredType) {
+        return applicationContext.getBean(name, requiredType);
+    }
+
+    /**
+     * 判断Bean是否存在.
+     */
+    public static boolean containsBean(String name) {
+        return applicationContext.containsBean(name);
+    }
+
+    /**
+     * 判断Bean是否单例.
+     */
+    public static boolean isSingleton(String name) {
+        return applicationContext.isSingleton(name);
+    }
+
+    /**
+     * 获取Bean类型.
+     */
+    public static Class<?> getType(String name) {
+        return applicationContext.getType(name);
+    }
+
+    /**
+     * 清除SpringContextHolder中的ApplicationContext为Null.
+     */
+    public static void clearHolder() {
+        applicationContext = null;
+    }
+
+    /**
+     * 发布事件.
+     */
+    public static void publishEvent(ApplicationEvent event) {
+        if (applicationContext == null) {
+            return;
+        }
+        applicationContext.publishEvent(event);
+    }
+
+    /**
+     * 实现DisposableBean接口, 在Context关闭时清理静态变量.
+     */
+    @Override
+    public void destroy() {
+        SpringContextHolder.clearHolder();
+    }
+}
