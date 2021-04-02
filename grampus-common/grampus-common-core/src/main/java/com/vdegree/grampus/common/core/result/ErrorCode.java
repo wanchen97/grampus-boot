180df2f15351abd4db586db80e2c62ead46ebe29
Beck <297513931@qq.com>
2021-04-02 10:42:21 +0800

    :recycle: 调整组件包.

--- grampus-common/grampus-common-core/src/main/java/com/vdegree/grampus/common/core/result/ErrorCode.java
+++ grampus-common/grampus-common-core/src/main/java/com/vdegree/grampus/common/core/result/ErrorCode.java
@@ -19,27 +19,29 @@ public interface ErrorCode {
      * errorCode
      */
     /** 基础错误码 */
-    String BASE_ERROR_CODE = "110000";
+    String BASE_ERROR_CODE = "BASE_ERROR_CODE";
     /** 接口入参通用异常 */
-    String BASE_PARAMS_ERROR = "110001";
+    String BASE_PARAMS_ERROR = "BASE_PARAMS_ERROR";
     /** 签名错误异常 */
-    String SIGNATURE_ERROR = "110002";
+    String SIGNATURE_ERROR = "SIGNATURE_ERROR";
     /** RPC调度异常 */
-    String RPC_INVOKE_ERROR = "110003";
+    String RPC_INVOKE_ERROR = "RPC_INVOKE_ERROR";
     /** 修改密码与原密码一致异常 */
-    String SAME_PASSWORD_ERROR = "110004";
+    String SAME_PASSWORD_ERROR = "SAME_PASSWORD_ERROR";
     /** 会员不存在异常 */
-    String MEMBER_NOT_EXISTED = "110005";
+    String MEMBER_NOT_EXISTED = "MEMBER_NOT_EXISTED";
     /** 密码错误异常 */
-    String MEMBER_PASSWORD_ERROR = "110006";
+    String MEMBER_PASSWORD_ERROR = "MEMBER_PASSWORD_ERROR";
     /** 验证码错误异常 */
-    String CAPTCHA_ERROR = "110007";
+    String CAPTCHA_ERROR = "CAPTCHA_ERROR";
     /** 会员账号冻结异常 */
-    String MEMBER_FREEZE_ERROR = "110008";
+    String MEMBER_FREEZE_ERROR = "MEMBER_FREEZE_ERROR";
     /** 对方会员账号冻结异常 */
-    String TARGET_MEMBER_FREEZE_ERROR = "110009";
+    String TARGET_MEMBER_FREEZE_ERROR = "TARGET_MEMBER_FREEZE_ERROR";
     /** 参数解析异常(加密接口) */
-    String PARAMS_DECODE_ERROR = "110010";
+    String PARAMS_DECODE_ERROR = "PARAMS_DECODE_ERROR";
     /** 会员已删除 */
-    String MEMBER_DELETED_ERROR = "110011";
+    String MEMBER_DELETED_ERROR = "MEMBER_DELETED_ERROR";
+
+    String SUB_MENU_EXIST = "SUB_MENU_EXIST";
 }
