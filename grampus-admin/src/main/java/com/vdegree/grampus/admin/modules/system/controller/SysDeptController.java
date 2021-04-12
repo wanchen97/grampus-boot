12b874f0519f4c0ced49187c42940354af1d78e6
Beck <297513931@qq.com>
2021-04-12 10:01:12 +0800

    :sparkles: 新增部门板块接口实现.

--- grampus-admin/src/main/java/com/vdegree/grampus/admin/modules/system/controller/SysDeptController.java
+++ grampus-admin/src/main/java/com/vdegree/grampus/admin/modules/system/controller/SysDeptController.java
@@ -6,7 +6,10 @@ import com.vdegree.grampus.admin.modules.system.service.SysDeptService;
 import com.vdegree.grampus.common.core.result.Result;
 import com.vdegree.grampus.common.core.utils.BeanUtil;
 import com.vdegree.grampus.admin.modules.system.utils.TreeUtils;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,6 +28,7 @@ import java.util.List;
  * @author Beck
  * @date 2021-01-21
  */
+@Api(tags = "部门管理")
 @RestController
 @RequestMapping("/dept")
 public class SysDeptController {
@@ -32,34 +36,44 @@ public class SysDeptController {
 	@Autowired
 	private SysDeptService sysDeptService;
 
+	@ApiOperation("部门列表")
 	@GetMapping("/list")
+	@PreAuthorize("hasAuthority('sys:dept:list')")
 	public Result<List<SysDeptDTO>> list() {
 		List<SysDept> list = sysDeptService.selectList(new SysDept());
 		List<SysDeptDTO> deptList = BeanUtil.copyList(list, SysDeptDTO.class);
 		return Result.success(TreeUtils.build(deptList));
 	}
 
+	@ApiOperation("部门信息")
 	@GetMapping("/{id}")
+	@PreAuthorize("hasAuthority('sys:dept:info')")
 	public Result<SysDeptDTO> get(@PathVariable Long id) {
 		SysDept sysDept = sysDeptService.selectById(id);
 		return Result.success(BeanUtil.copy(sysDept, SysDeptDTO.class));
 	}
 
+	@ApiOperation("部门保存")
 	@PostMapping("/save")
+	@PreAuthorize("hasAuthority('sys:dept:save')")
 	public Result<Void> save(@RequestBody SysDeptDTO sysDeptDTO) {
 		SysDept sysDept = BeanUtil.copy(sysDeptDTO, SysDept.class);
 		sysDeptService.insert(sysDept);
 		return Result.success();
 	}
 
+	@ApiOperation("部门更新")
 	@PutMapping("/update")
+	@PreAuthorize("hasAuthority('sys:dept:update')")
 	public Result<Void> update(@RequestBody SysDeptDTO sysDeptDTO) {
 		SysDept sysDept = BeanUtil.copy(sysDeptDTO, SysDept.class);
 		sysDeptService.updateById(sysDept);
 		return Result.success();
 	}
 
+	@ApiOperation("部门删除")
 	@DeleteMapping("/{id}")
+	@PreAuthorize("hasAuthority('sys:dept:delete')")
 	public Result<Void> delete(@PathVariable Long id) {
 		sysDeptService.deleteById(id);
 		return Result.success();
