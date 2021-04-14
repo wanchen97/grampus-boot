6cde409a6a6ea4edb84525fa2a01ddc57721c096
Beck <297513931@qq.com>
2021-04-14 15:54:44 +0800

    :recycle: 分页查询参数调整.

--- grampus-common/grampus-common-mybatis/src/main/java/com/vdegree/grampus/common/mybatis/page/PageQuery.java
+++ grampus-common/grampus-common-mybatis/src/main/java/com/vdegree/grampus/common/mybatis/page/PageQuery.java
@@ -1,5 +1,7 @@
 package com.vdegree.grampus.common.mybatis.page;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.io.Serializable;
@@ -11,23 +13,33 @@ import java.io.Serializable;
  * @author Beck
  * @date 2021-04-12
  */
+@ApiModel("分页查询参数")
 @Data
 public class PageQuery implements Serializable {
 	private static final long serialVersionUID = 5153822821409425132L;
 	/**
 	 * 当前页数
 	 */
+	@ApiModelProperty("当前页数")
 	protected Integer pageNum;
 	/**
 	 * 每页数据量
 	 */
+	@ApiModelProperty("每页数据量")
 	protected Integer pageSize;
 	/**
 	 * 排序字段
 	 */
+	@ApiModelProperty("排序字段")
 	protected String orderField;
 	/**
 	 * 排序（asc升序 desc倒序）
 	 */
+	@ApiModelProperty("排序（asc升序 desc倒序）")
 	protected String order;
+	/**
+	 * 是否查count（true查count false不查count）
+	 */
+	@ApiModelProperty("是否查count（true查count false不查count）")
+	protected Boolean withCount;
 }
