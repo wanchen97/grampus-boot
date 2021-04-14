2228b2b65d1ffc6e1e86ad80713e91472dbaff66
Beck <297513931@qq.com>
2021-04-14 15:46:09 +0800

    :art: 分页数据模型提交.

--- grampus-common/grampus-common-mybatis/src/main/java/com/vdegree/grampus/common/mybatis/page/PageData.java
+++ grampus-common/grampus-common-mybatis/src/main/java/com/vdegree/grampus/common/mybatis/page/PageData.java
@@ -1,6 +1,8 @@
 package com.vdegree.grampus.common.mybatis.page;
 
 import com.github.pagehelper.PageInfo;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.util.List;
@@ -12,31 +14,38 @@ import java.util.List;
  * @author Beck
  * @date 2021-04-12
  */
+@ApiModel("分页数据结构")
 @Data
 public class PageData<T> {
 	/**
 	 * 当前页
 	 */
+	@ApiModelProperty("当前页")
 	private int pageNum;
 	/**
 	 * 每页的数量
 	 */
+	@ApiModelProperty("每页的数量")
 	private int pageSize;
 	/**
 	 * 总页数
 	 */
+	@ApiModelProperty("总页数")
 	private int pages;
 	/**
 	 * 是否为最后一页
 	 */
+	@ApiModelProperty("是否为最后一页")
 	private boolean lastPage;
 	/**
 	 * 总记录数
 	 */
+	@ApiModelProperty("总记录数")
 	private long total;
 	/**
 	 * 列表数据
 	 */
+	@ApiModelProperty("列表数据")
 	private List<T> list;
 
 	public PageData() {
