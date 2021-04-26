95e62824909df0e7929b215cf44379ed3253a497
Beck <297513931@qq.com>
2021-04-26 20:55:04 +0800

    :refactor: 调整mybatis配置方式..

--- grampus-admin/src/main/java/com/vdegree/grampus/admin/modules/system/config/MybatisConfig.java
+++ grampus-admin/src/main/java/com/vdegree/grampus/admin/modules/system/config/MybatisConfig.java
@@ -1,17 +1,9 @@
 package com.vdegree.grampus.admin.modules.system.config;
 
-import com.vdegree.grampus.admin.modules.system.security.utils.SecurityUtils;
-import com.vdegree.grampus.common.mybatis.annotation.FieldFill;
-import com.vdegree.grampus.common.mybatis.annotation.TableField;
 import com.vdegree.grampus.common.mybatis.handler.FieldFillHandler;
 import com.vdegree.grampus.common.mybatis.interceptor.FieldFillInterceptor;
-import org.apache.ibatis.mapping.SqlCommandType;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import java.lang.reflect.Field;
-import java.util.Date;
-import java.util.List;
-import java.util.Objects;
 
 /**
  * Title: 字段填充处理器
@@ -22,50 +14,9 @@ import java.util.Objects;
  */
 @Configuration
 public class MybatisConfig {
-	private final static String CREATE_BY = "createBy";
-	private final static String CREATE_DATE = "createDate";
-	private final static String UPDATE_BY = "updateBy";
-	private final static String UPDATE_DATE = "updateDate";
 
 	@Bean
 	public FieldFillInterceptor fieldFillInterceptor(FieldFillHandler fieldFillHandler) {
 		return new FieldFillInterceptor(fieldFillHandler);
-	}
-
-	@Bean
-	public FieldFillHandler fieldFillHandler() {
-		return (SqlCommandType sqlCommandType, List<Field> fields, Object paramObj) -> {
-			Long currentUserId = Objects.requireNonNull(SecurityUtils.getUserDetails()).getId();
-			Date currentDate = new Date();
-			for (Field field : fields) {
-				field.setAccessible(true);
-				TableField tableField = field.getAnnotation(TableField.class);
-				if (SqlCommandType.INSERT.equals(sqlCommandType)) {
-					// INSERT SQL
-					if (FieldFill.INSERT.equals(tableField.fill())
-							|| FieldFill.INSERT_UPDATE.equals(tableField.fill())) {
-						if (CREATE_BY.equals(field.getName())) {
-							field.set(paramObj, currentUserId);
-						} else if (CREATE_DATE.equals(field.getName())) {
-							field.set(paramObj, currentDate);
-						} else if (UPDATE_BY.equals(field.getName())) {
-							field.set(paramObj, currentUserId);
-						} else if (UPDATE_DATE.equals(field.getName())) {
-							field.set(paramObj, currentDate);
-						}
-					}
-				} else if (SqlCommandType.UPDATE.equals(sqlCommandType)) {
-					// UPDATE SQL
-					if (FieldFill.UPDATE.equals(tableField.fill())
-							|| FieldFill.INSERT_UPDATE.equals(tableField.fill())) {
-						if (UPDATE_BY.equals(field.getName())) {
-							field.set(paramObj, currentUserId);
-						} else if (UPDATE_DATE.equals(field.getName())) {
-							field.set(paramObj, currentDate);
-						}
-					}
-				}
-			}
-		};
 	}
 }
---
+++ grampus-admin/src/main/java/com/vdegree/grampus/admin/modules/system/handler/FieldAutoFillHandler.java
@@ -0,0 +1,63 @@
+package com.vdegree.grampus.admin.modules.system.handler;
+
+import com.vdegree.grampus.admin.modules.system.security.utils.SecurityUtils;
+import com.vdegree.grampus.common.mybatis.annotation.FieldFill;
+import com.vdegree.grampus.common.mybatis.annotation.TableField;
+import com.vdegree.grampus.common.mybatis.handler.FieldFillHandler;
+import org.apache.ibatis.mapping.SqlCommandType;
+import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Field;
+import java.util.Date;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * Title: 字段自动填充处理器
+ * Company: v-degree
+ *
+ * @author Beck
+ * @date 2021-04-26
+ */
+@Component
+public class FieldAutoFillHandler implements FieldFillHandler {
+	private final static String CREATE_BY = "createBy";
+	private final static String CREATE_DATE = "createDate";
+	private final static String UPDATE_BY = "updateBy";
+	private final static String UPDATE_DATE = "updateDate";
+
+	@Override
+	public void fillField(SqlCommandType sqlCommandType, List<Field> fields, Object paramObj) throws IllegalAccessException {
+		Long currentUserId = Objects.requireNonNull(SecurityUtils.getUserDetails()).getId();
+		Date currentDate = new Date();
+		for (Field field : fields) {
+			field.setAccessible(true);
+			TableField tableField = field.getAnnotation(TableField.class);
+			if (SqlCommandType.INSERT.equals(sqlCommandType)) {
+				// INSERT SQL
+				if (FieldFill.INSERT.equals(tableField.fill())
+						|| FieldFill.INSERT_UPDATE.equals(tableField.fill())) {
+					if (CREATE_BY.equals(field.getName())) {
+						field.set(paramObj, currentUserId);
+					} else if (CREATE_DATE.equals(field.getName())) {
+						field.set(paramObj, currentDate);
+					} else if (UPDATE_BY.equals(field.getName())) {
+						field.set(paramObj, currentUserId);
+					} else if (UPDATE_DATE.equals(field.getName())) {
+						field.set(paramObj, currentDate);
+					}
+				}
+			} else if (SqlCommandType.UPDATE.equals(sqlCommandType)) {
+				// UPDATE SQL
+				if (FieldFill.UPDATE.equals(tableField.fill())
+						|| FieldFill.INSERT_UPDATE.equals(tableField.fill())) {
+					if (UPDATE_BY.equals(field.getName())) {
+						field.set(paramObj, currentUserId);
+					} else if (UPDATE_DATE.equals(field.getName())) {
+						field.set(paramObj, currentDate);
+					}
+				}
+			}
+		}
+	}
+}
