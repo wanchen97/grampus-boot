7d41a085b2e23a9268f68fe4164572cec6beba94
Beck <297513931@qq.com>
2021-04-29 20:10:12 +0800

    :wrench: JUnit添加Spring环境启动.

--- grampus-admin/src/test/java/com/vdegree/grampus/common/security/test/JwtTest.java
+++ grampus-admin/src/test/java/com/vdegree/grampus/common/security/test/JwtTest.java
@@ -1,7 +1,11 @@
 package com.vdegree.grampus.common.security.test;
 
+import com.vdegree.grampus.admin.GrampusBootAdminApplication;
+import com.vdegree.grampus.common.redis.utils.RedisCache;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 /**
  * Title:
@@ -11,6 +15,7 @@ import org.junit.jupiter.api.Test;
  * @date 2020-12-09
  */
 @Slf4j
+@SpringBootTest(classes = GrampusBootAdminApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class JwtTest {
 
 	@Test
@@ -18,5 +23,15 @@ public class JwtTest {
 		String bearerToken = "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJIMDAxIiwiZXhwIjoxNjA4MzUzMzI3fQ.8QIKHqeWWh8rq32CVNG20-6vkwbV9-wVlqCz5LrVegdaHRNbJZO0asiIhMpha1_EhL2sxscyBL-nxX0LJvkGtQ";
 		String result = bearerToken.substring(7);
 		log.debug("result:{}", result);
+	}
+
+	@Autowired
+	private RedisCache redisCache;
+
+	@Test
+	public void test2() {
+		redisCache.hSet("test_hset_key", "testField", "1111111");
+		redisCache.expire("test_hset_key", 0);
+		log.info("..........................");
 	}
 }
