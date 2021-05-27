b1e9a1b7faf709c4f72e2b894be84aed481bca4c
Beck <297513931@qq.com>
2021-05-27 10:57:26 +0800

    :fire: 添加新的超管用户.

--- doc/grampus_system.sql
+++ doc/grampus_system.sql
@@ -273,6 +273,7 @@ INSERT INTO `sys_menu` VALUES (35784998272385024, 35784905746038784, 1, '字典�
 -- Records of sys_user
 -- ----------------------------
 INSERT INTO `sys_user` VALUES (126742336940032000, 'H001', 'H001', '$2a$10$Blav.U6cFEu7/1ONBeRAoudAQK12bA0qnysGEcl9CIOq.b4L7E94m', NULL, NULL, 1, NULL, NULL, '2020-12-15 09:48:47.812', NULL, '2020-12-15 09:48:47.812', 0);
+INSERT INTO `sys_user` VALUES (126742336940033000, 'H002', 'H002', '$2a$10$Blav.U6cFEu7/1ONBeRAoudAQK12bA0qnysGEcl9CIOq.b4L7E94m', NULL, NULL, 1, NULL, NULL, '2020-12-15 09:48:47.812', NULL, '2020-12-15 09:48:47.812', 0);
 
 -- ----------------------------
 -- Records of sys_user_role
