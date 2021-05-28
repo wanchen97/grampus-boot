64897ac510248fd2348fa8d42be286317e2a2cc6
Beck <297513931@qq.com>
2021-05-28 17:34:02 +0800

    :wrench: 优化日志组件.

--- grampus-common/grampus-common-log/src/main/java/com/vdegree/grampus/common/log/aspect/LogInterceptor.java
+++ grampus-common/grampus-common-log/src/main/java/com/vdegree/grampus/common/log/aspect/LogInterceptor.java
@@ -1,24 +1,17 @@
 package com.vdegree.grampus.common.log.aspect;
 
-import com.vdegree.grampus.common.core.utils.ClassUtil;
 import com.vdegree.grampus.common.core.utils.Exceptions;
-import com.vdegree.grampus.common.core.utils.JSONUtil;
 import com.vdegree.grampus.common.core.utils.chars.StringPool;
-import com.vdegree.grampus.common.core.utils.spring.SpringContextHolder;
 import com.vdegree.grampus.common.log.annotation.RequestLog;
 import com.vdegree.grampus.common.log.event.LogEvent;
+import com.vdegree.grampus.common.log.factory.LogEventFactory;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.aopalliance.intercept.MethodInterceptor;
 import org.aopalliance.intercept.MethodInvocation;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
-import org.springframework.core.MethodParameter;
-import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.core.env.Environment;
-import org.springframework.web.bind.annotation.RequestBody;
 
-import java.lang.reflect.Method;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -42,11 +35,10 @@ public class LogInterceptor implements MethodInterceptor {
 		String strClassName = invocation.getThis().getClass().getName();
 		String strMethodName = invocation.getMethod().getName();
 		log.info("[class]:{},[method]:{}", strClassName, strMethodName);
-		LogEvent event = new LogEvent();
+		LogEvent event = LogEventFactory.buildLogEvent(invocation);
 		event.setModule(applicationName);
 		event.setDescription(requestLog.value());
 		event.setClassMethod(strClassName + StringPool.HASH + strMethodName);
-		event.setData(getPostJson(invocation));
 		// 执行时间
 		long startNs = System.nanoTime();
 		try {
@@ -67,23 +59,5 @@ public class LogInterceptor implements MethodInterceptor {
 			applicationContext.publishEvent(event);
 			throw e;
 		}
-	}
-
-	private static String getPostJson(MethodInvocation invocation) {
-		Object[] args = invocation.getArguments();
-		Method method = invocation.getMethod();
-		// 一次请求只能有一个 request body
-		Object requestBodyValue = null;
-		for (int i = 0; i < args.length; i++) {
-			// 读取方法参数
-			MethodParameter methodParam = ClassUtil.getMethodParameter(method, i);
-			RequestBody requestBody = methodParam.getParameterAnnotation(RequestBody.class);
-			// 如果是body的json则是对象
-			if (requestBody != null) {
-				requestBodyValue = args[i];
-				break;
-			}
-		}
-		return requestBodyValue == null ? null : JSONUtil.writeValueAsString(requestBodyValue);
 	}
 }
---
+++ grampus-common/grampus-common-log/src/main/java/com/vdegree/grampus/common/log/factory/LogEventFactory.java
@@ -0,0 +1,82 @@
+package com.vdegree.grampus.common.log.factory;
+
+import com.vdegree.grampus.common.core.utils.ClassUtil;
+import com.vdegree.grampus.common.core.utils.JSONUtil;
+import com.vdegree.grampus.common.core.utils.ObjectUtil;
+import com.vdegree.grampus.common.core.utils.StringUtil;
+import com.vdegree.grampus.common.core.utils.WebUtil;
+import com.vdegree.grampus.common.core.utils.chars.CharPool;
+import com.vdegree.grampus.common.core.utils.chars.StringPool;
+import com.vdegree.grampus.common.log.event.LogEvent;
+import org.aopalliance.intercept.MethodInvocation;
+import org.springframework.core.MethodParameter;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import javax.servlet.http.HttpServletRequest;
+import java.lang.reflect.Method;
+import java.util.Map;
+
+/**
+ * 系统日志构建工厂类
+ *
+ * @author Beck
+ * @since 2021-05-28
+ */
+public class LogEventFactory {
+
+	/**
+	 * 构造 LogEvent
+	 *
+	 * @return LogEvent
+	 */
+	public static LogEvent buildLogEvent(MethodInvocation invocation) {
+		LogEvent event = new LogEvent();
+		HttpServletRequest request = WebUtil.getRequest();
+		String method = request.getMethod();
+		// 请求信息 GET /api/test/xx
+		String requestInfo = method + StringPool.SPACE + request.getRequestURI();
+		// paramMap
+		Map<String, String[]> paraMap = request.getParameterMap();
+		if (ObjectUtil.isEmpty(paraMap)) {
+			event.setParams(requestInfo);
+		} else {
+			StringBuilder builder = new StringBuilder(requestInfo).append(CharPool.QUESTION_MARK);
+			paraMap.forEach((key, values) -> {
+				builder.append(key).append(CharPool.EQUALS);
+				if ("password".equalsIgnoreCase(key)) {
+					builder.append("******");
+				} else {
+					builder.append(StringUtil.join(values));
+				}
+				builder.append(CharPool.AMPERSAND);
+			});
+			builder.deleteCharAt(builder.length() - 1);
+			event.setParams(builder.toString());
+		}
+		// 获取请求 ip 和 ua
+		event.setRequestIp(WebUtil.getIP());
+		event.setUserAgent(request.getHeader(HttpHeaders.USER_AGENT));
+		// 设置post json数据
+		event.setData(getPostJson(invocation));
+		return event;
+	}
+
+	private static String getPostJson(MethodInvocation invocation) {
+		Object[] args = invocation.getArguments();
+		Method method = invocation.getMethod();
+		// 一次请求只能有一个 request body
+		Object requestBodyValue = null;
+		for (int i = 0; i < args.length; i++) {
+			// 读取方法参数
+			MethodParameter methodParam = ClassUtil.getMethodParameter(method, i);
+			RequestBody requestBody = methodParam.getParameterAnnotation(RequestBody.class);
+			// 如果是body的json则是对象
+			if (requestBody != null) {
+				requestBodyValue = args[i];
+				break;
+			}
+		}
+		return requestBodyValue == null ? null : JSONUtil.writeValueAsString(requestBodyValue);
+	}
+}
