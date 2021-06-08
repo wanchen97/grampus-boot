0a195c2854a626a774a5898a873b88a9453c3b60
Alberich <297513931@qq.com>
2021-06-08 17:53:06 +0800

    :sparkles: 添加临时测试接口.

---
+++ grampus-admin/src/main/java/com/vdegree/grampus/admin/modules/system/controller/SysTestController.java
@@ -0,0 +1,43 @@
+package com.vdegree.grampus.admin.modules.system.controller;
+
+import com.vdegree.grampus.admin.modules.system.service.SysLanguageService;
+import com.vdegree.grampus.common.core.result.Result;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.AllArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 系统测试管理
+ *
+ * @author Beck
+ * @since 2021-06-08
+ */
+@Api(tags = "测试管理")
+@AllArgsConstructor
+@RestController
+@RequestMapping("/system/test")
+public class SysTestController {
+
+	private final SysLanguageService sysLanguageService;
+
+	@PostMapping("/lang")
+	@ApiOperation("测试多语言")
+	@PreAuthorize("hasAuthority('sys:user:info')")
+	public Result<Map<Long,String>> get(@RequestBody List<Long> tableIds) {
+		Map<Long,String> result = sysLanguageService.convertFieldValue("sys_menu", tableIds, "menu_name", "en-US");
+		return Result.success(result);
+	}
+
+	@PostMapping("/lang1")
+	@ApiOperation("测试多语言")
+	@PreAuthorize("hasAuthority('sys:user:info')")
+	public Result<String> get(@RequestBody Long tableId) {
+		String result = sysLanguageService.convertFieldValue("sys_menu", tableId, "menu_name", "en-US");
+		return Result.success(result);
+	}
+}
